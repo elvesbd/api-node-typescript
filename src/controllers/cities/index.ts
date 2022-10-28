d42bde5d5df13ffc558b50acbec4e86106a79453
elvesbd <elvesbd@yahoo.com>
2022-10-28 20:50:39 -0300

    feat: add route remove

--- src/controllers/cities/index.ts
+++ src/controllers/cities/index.ts
@@ -1,11 +1,13 @@
 import * as create  from './create';
 import * as getAll from './get-all';
 import * as getById from './get-by-id';
+import * as remove from './remove';
 import * as update from './update';
 
 export const CitiesController = {
   ...create,
   ...getAll,
   ...getById,
-  ...update
+  ...update,
+  ...remove
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,5 +8,6 @@ route.post('/cities', CitiesController.createValidation, CitiesController.create
 route.get('/cities', CitiesController.getAllValidation, CitiesController.getAll);
 route.get('/cities/:id', CitiesController.getByIdValidation, CitiesController.getById);
 route.put('/cities/:id', CitiesController.updateValidation, CitiesController.update);
+route.put('/cities/:id', CitiesController.removeValidation, CitiesController.remove);
 
 export { route };
