2ab5deda8f214a1f3de133983b8bc161287bea28
elvesbd <elvesbd@yahoo.com>
2022-11-21 09:38:45 -0300

    tests: add unit test in method get all cities

---
+++ tests/cities/get-all.test.ts
@@ -0,0 +1,21 @@
+import { StatusCodes } from 'http-status-codes';
+import { request } from '../jest.setup';
+
+describe('Cities - GetAll', () => {
+  it('should be an get all registers', async () => {
+    await request
+      .post('/cities')
+      .send({
+        name: 'Caxias do Sul'
+      });
+
+    const sut = await request
+      .get('/cities')
+      .send();
+
+    expect(Number(sut.header['x-total-count'])).toBeGreaterThan(0);
+    expect(sut.statusCode).toBe(StatusCodes.OK);
+    expect(sut.body.length).toBeGreaterThan(0);
+  });
+});
+
