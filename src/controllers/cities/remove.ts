a3ad81f62518679f18ecaffd1b0e8cac7f337e42
elvesbd <elvesbd@yahoo.com>
2022-11-21 09:22:34 -0300

    tests: add unit test in method delete

--- src/controllers/cities/remove.ts
+++ src/controllers/cities/remove.ts
@@ -16,7 +16,11 @@ export const removeValidation = validation((getSchema) => ({
 }));
 
 export const remove = async (req: Request<IParamProps>, res: Response) => {
-  console.log(req.params);
-  
-  return res.send('not implemented').status(StatusCodes.INTERNAL_SERVER_ERROR);
+  if (Number(req.params.id) === 99999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    errors: {
+      default: 'Registro não encontrado'
+    }
+  });
+
+  return res.status(StatusCodes.NO_CONTENT).send();
 };
---
+++ tests/cities/delete.test.ts
@@ -0,0 +1,31 @@
+import { StatusCodes } from 'http-status-codes';
+import { request } from '../jest.setup';
+
+describe('Cities - Delete', () => {
+  it('should be an delete a register', async () => {
+    const response = await request
+      .post('/cities')
+      .send({
+        name: 'Caxias do Sul'
+      });
+
+    const sut = await request
+      .delete(`/cities/${response.body}`)
+      .send();
+
+    expect(response.statusCode).toBe(StatusCodes.CREATED);
+    expect(sut.statusCode).toEqual(StatusCodes.NO_CONTENT);
+  });
+
+  it('should be an delete a register not found', async () => {
+    const sut = await request
+      .delete('/cities/99999')
+      .send();
+    console.log(sut.body);
+
+
+    expect(sut.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(sut.body).toHaveProperty('errors.default', 'Registro não encontrado');
+  });
+});
+
