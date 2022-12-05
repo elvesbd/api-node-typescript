8128a909d116b90ed9c26fcba642807e3228ae0d
Elves Brito <elvesbd@yahoo.com>
2022-12-05 14:11:00 -0300

    refactor: implemented controller create

--- src/controllers/cities/create.ts
+++ src/controllers/cities/create.ts
@@ -3,17 +3,27 @@ import { StatusCodes } from 'http-status-codes';
 import { validation } from '../../shared/middlewares';
 import * as yup from 'yup';
 import { ICity } from '../../database/models';
+import { CitiesProvider } from '../../database/providers/cities';
 
 interface IBodyProps extends Omit<ICity, 'id'> {}
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(
     yup.object().shape({
-      name: yup.string().required().min(3),
+      name: yup.string().required().min(3).max(150),
     })
   ),
 }));
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
-  return res.status(StatusCodes.CREATED).json(1);
+  const response = await CitiesProvider.create(req.body);
+
+  if (response instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: response.message,
+      }
+    });
+  }
+  return res.status(StatusCodes.CREATED).json(response);
 };
